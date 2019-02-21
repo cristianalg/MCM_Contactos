911d78f0bb4ff499f17e44b7f22cfe8eb657ca28
Cristiana <cristianalg7@hotmail.com>
2019-02-21 18:04:50 +0000

    Layout's da app
    Definidos os layout's das atividades da app.

---
+++ app/src/main/java/ipt/ipg/contactos/ContactDetailsActivity.java
@@ -0,0 +1,13 @@
+package ipt.ipg.contactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_details);
+    }
+}
---
+++ app/src/main/java/ipt/ipg/contactos/NewContactActivity.java
@@ -0,0 +1,13 @@
+package ipt.ipg.contactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_contact);
+    }
+}
