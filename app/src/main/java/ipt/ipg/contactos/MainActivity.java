31cdfb9c170fc842b9e7f79a5f3a5541e5f0de18
Cristiana <cristianalg7@hotmail.com>
2019-02-28 18:28:06 +0000

    Artigo
    Versão 1 do artigo.

--- app/src/main/java/ipt/ipg/contactos/MainActivity.java
+++ app/src/main/java/ipt/ipg/contactos/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -16,13 +17,13 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ContactHandler handler;
+    private List<Contact> contacts;
     ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         handler = new ContactHandler(getApplicationContext());
 
         Button btn_add_new = (Button) findViewById(R.id.btn_add_contact);
@@ -30,21 +31,49 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-                // TODO Auto-generated method stub
+
                 Intent intent = new Intent(MainActivity.this, NewContactActivity.class);
                 startActivity(intent);
             }
         });
 
         lv = (ListView) findViewById(R.id.lv_contact_list);
+
+
         loadContactData();
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+                Intent intent = new Intent(MainActivity.this, ContactDetailsActivity.class);
+                intent.putExtra("id", contacts.get(position).getID());
+                intent.putExtra("name", contacts.get(position).getName());
+                intent.putExtra("phone", contacts.get(position).getPhoneNumber());
+                intent.putExtra("email", contacts.get(position).getEmail());
+                intent.putExtra("address", contacts.get(position).getPostalAddress());
+                intent.putExtra("photograph", contacts.get(position).getPhotograph());
+                startActivity(intent);
+
+
+            }
+
+        });
+
+
+
     }
 
 
     private void loadContactData(){
+
+
         // Code for loading contact list in ListView
         // Reading all contacts
-        List<Contact> contacts = handler.readAllContacts();
+        contacts = handler.readAllContacts();
 
         // Initialize Custom Adapter
         CustomAdapter adapter = new CustomAdapter(this, contacts);
@@ -52,11 +81,15 @@ public class MainActivity extends AppCompatActivity {
         // Set Adapter to ListView
         lv.setAdapter(adapter);
 
+        // See the log int LogCat
         for(Contact c : contacts){
             String record = "ID=" + c.getID() + " | Name=" + c.getName() + " | " + c.getPhoneNumber();
-
             Log.d("Record",record);
+
+
         }
+
+
     }
 
 
